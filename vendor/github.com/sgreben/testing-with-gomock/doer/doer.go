dd26c41bb0716f827c11fc626808fcff92cef692
Igor Pestana <igor.pestana@m4u.com.br>
2020-08-08 15:56:38 -0300

    teste deploy

--- vendor/github.com/sgreben/testing-with-gomock/doer/doer.go
+++
@@ -1,7 +0,0 @@
-package doer
-
-//go:generate mockgen -destination=../mocks/mock_doer.go -package=mocks github.com/sgreben/testing-with-gomock/doer Doer
-
-type Doer interface {
-	DoSomething(int, string) error
-}
